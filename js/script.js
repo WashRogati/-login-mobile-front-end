ab6b83258d4fdf2a9a6d5c6db840c3a1ff19835e
WashRogati <wrogati@gmail.com>
2023-03-26 10:59:45 -0300

    Feita a conexão entre as telas de menu-login, e sign-up

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ function Error(){
 
 window.onload = function() {
     var div = document.querySelector(".mobile-ui");
-    var input = document.querySelector(".btn");
+    var btn = document.querySelector(".btn");
+    var signup = document.querySelector(".sign-up")
 
-    input.onclick = function() {
+    btn.onclick = function() {
          div.style.display = "none";
+         signup.style.display = "flex";
+
     }
 }
