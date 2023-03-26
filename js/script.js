b28cce82d91ffab7a7961fc8a78186e690f186dc
WashRogati <wrogati@gmail.com>
2023-03-26 16:31:41 -0300

    Javascript configurado

--- js/script.js
+++ js/script.js
@@ -2,14 +2,37 @@ function Error(){
     alert("NAO FUNCIONA AINDA!")
 }
 
-window.onload = function() {
+btns = function() {
+    
     var div = document.querySelector(".mobile-ui");
-    var btn = document.querySelector(".btn");
-    var signup = document.querySelector(".sign-up")
+    var btn_signup = document.querySelector("#btnsignup");
+    var btn_signin = document.querySelector("#btnsignin")
+    var btn_signupi = document.querySelector("#btnsignupi");
+    var btn_signini = document.querySelector("#btnsignini")
+    var signup = document.querySelector(".sign-up");
+    var signin = document.querySelector(".sign-in");
 
-    btn.onclick = function() {
+    
+    btn_signup.onclick = function register() {
          div.style.display = "none";
          signup.style.display = "flex";
-
+         signin.style.display = "none";
     }
+
+    btn_signin.onclick = function login() {
+        div.style.display = "none";
+        signin.style.display = "flex";
+        signup.style.display = "none";
+   }
+    btn_signupi.onclick = function register() {
+         div.style.display = "none";
+         signup.style.display = "flex";
+         signin.style.display = "none";
+    }
+
+    btn_signini.onclick = function login() {
+        div.style.display = "none";
+        signin.style.display = "flex";
+        signup.style.display = "none";
+   }
 }
